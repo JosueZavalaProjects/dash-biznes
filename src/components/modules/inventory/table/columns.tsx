2cbd9da68a43e40b3dc1d2231cddd2a7f093d527
Josue Zavala <josue.zavala.projects@gmail.com>
2024-03-06 15:47:46 -0600

    Correcting Inventory columns

--- src/components/modules/inventory/table/columns.tsx
+++ src/components/modules/inventory/table/columns.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
-
 import { Product } from "@/types/inventory";
 import { formatCurrency } from "@/utils/common";
-import { lorelei } from "@dicebear/collection";
-import { createAvatar } from "@dicebear/core";
 import { ColumnDef } from "@tanstack/react-table";
 
 export const InventoryColumns: ColumnDef<Product>[] = [
@@ -11,21 +7,9 @@ export const InventoryColumns: ColumnDef<Product>[] = [
     accessorKey: "name",
     header: "Nombre",
     cell: ({ row }) => {
-      const avatar = createAvatar(lorelei, {
-        seed: row.getValue("name"),
-        backgroundColor: ["b6e3f4", "c0aede", "d1d4f9"],
-      });
-
-      const svg = avatar.toDataUriSync();
       return (
         <div className="flex gap-2 items-center">
-          <Image
-            width={60}
-            height={60}
-            className="h-15 w-15 bg-main-gray rounded-full"
-            src={svg}
-            alt="user-image"
-          />
+          <span className="w-14 h-14 bg-gray-400 rounded-full"></span>
           <p>{row.getValue("name")} </p>
         </div>
       );
