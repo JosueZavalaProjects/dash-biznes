8c6fe29546fccd2ac195973ac2b3c79b74731595
Josue Zavala <josue.zavala.projects@gmail.com>
2024-07-03 04:16:43 -0600

    Creating new components NavBar

---
+++ src/components/NavBar/NavBarOptions.tsx
@@ -0,0 +1,50 @@
+import {
+  ActivitiesIcon,
+  ActivitiesWhiteIcon,
+  DashboardIcon,
+  DashboardWhiteIcon,
+  InventoryIcon,
+  InventoryWhiteIcon,
+  SalesPointIcon,
+  SalesPointWhiteIcon,
+} from "../../../public/assets";
+import { NavOption } from "./NavOption";
+
+export const NavBarOptions = ({ pathname }: { pathname: string }) => {
+  return (
+    <>
+      <NavOption
+        text="Dashboard"
+        icon={{ image: DashboardIcon }}
+        selectedIcon={{ image: DashboardWhiteIcon }}
+        link="/"
+        isSelected={pathname === "/"}
+      />
+      <NavOption
+        text="Actividades"
+        icon={{ image: ActivitiesIcon }}
+        selectedIcon={{ image: ActivitiesWhiteIcon }}
+        link="/activities"
+        isSelected={pathname === "/activities"}
+      />
+      <NavOption
+        text="Inventario"
+        icon={{ image: InventoryIcon }}
+        selectedIcon={{ image: InventoryWhiteIcon }}
+        link="/inventory"
+        isSelected={pathname === "/inventory"}
+      />
+      <NavOption
+        text="Punto de venta"
+        icon={{ image: SalesPointIcon, width: 30, height: 30 }}
+        selectedIcon={{
+          image: SalesPointWhiteIcon,
+          width: 30,
+          height: 30,
+        }}
+        link="/salesPoint"
+        isSelected={pathname === "/salesPoint"}
+      />
+    </>
+  );
+};
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,22 +1,15 @@
-import type { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-import {
-  ActivitiesIcon,
-  ActivitiesWhiteIcon,
-  BiznesLogo,
-  DashboardIcon,
-  DashboardWhiteIcon,
-  InventoryIcon,
-  InventoryWhiteIcon,
-  LogoutIcon,
-  SalesPointIcon,
-  SalesPointWhiteIcon,
-  SettingsIcon,
-} from "../../../public/assets";
+import { BiznesLogo, LogoutIcon, SettingsIcon } from "../../../public/assets";
+import { BusinessCard } from "./BusinessCard";
+import { NavBarOptions } from "./NavBarOptions";
+import { NavOption } from "./NavOption";
 
 export const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <nav className="flex flex-col h-full justify-between text-secondary-gray">
       <div className="flex flex-col  gap-4">
@@ -28,31 +21,7 @@ export const Navbar = () => {
         <div className="flex flex-col gap-4">
           <div className="">Menu</div>
           <div className="pl-4">
-            <NavOption
-              text="Dashboard"
-              icon={{ image: DashboardIcon }}
-              selectedIcon={{ image: DashboardWhiteIcon }}
-              isSelected
-            />
-            <NavOption
-              icon={{ image: ActivitiesIcon }}
-              selectedIcon={{ image: ActivitiesWhiteIcon }}
-              text="Actividades"
-            />
-            <NavOption
-              icon={{ image: InventoryIcon }}
-              selectedIcon={{ image: InventoryWhiteIcon }}
-              text="Inventario"
-            />
-            <NavOption
-              icon={{ image: SalesPointIcon, width: 30, height: 30 }}
-              selectedIcon={{
-                image: SalesPointWhiteIcon,
-                width: 30,
-                height: 30,
-              }}
-              text="Punto de venta"
-            />
+            <NavBarOptions pathname={pathname} />
           </div>
         </div>
       </div>
@@ -65,60 +34,9 @@ export const Navbar = () => {
           </div>
         </div>
         <div>
-          <div className="flex w-full h-22 bg-gradient-to-r from-secondary-light-blue to-main-blue/70 rounded-lg">
-            <div className="grid justify-items-center items-center p-4">
-              <span className="grid justify-items-center items-center w-14 h-14 rounded-full bg-white">
-                Img
-              </span>
-            </div>
-            <div className="flex flex-col justify-center">
-              <div className="text-third-blue font-light">Negocio</div>
-              <div className="text-cadet-grey">Ver el perfil &gt; </div>
-            </div>
-          </div>
+          <BusinessCard />
         </div>
       </div>
     </nav>
-  );
-};
-
-type IconType = {
-  image: StaticImport;
-  width?: number | `${number}`;
-  height?: number | `${number}`;
-};
-
-type NavOptionProps = {
-  text: string;
-  icon: IconType;
-  selectedIcon?: IconType;
-  isSelected?: boolean;
-};
-
-const NavOption = ({
-  isSelected,
-  text,
-  icon,
-  selectedIcon = icon,
-}: NavOptionProps) => {
-  return (
-    <div
-      className={`flex w-56 gap-2 p-4 rounded-3xl font-semibold cursor-pointer ${
-        isSelected ? "bg-main-blue text-white" : ""
-      }`}
-    >
-      <Link href={"/"}>
-        <span>
-          <Image
-            src={isSelected ? selectedIcon.image : icon.image}
-            width={selectedIcon.width || 21}
-            height={selectedIcon.width || 21}
-            alt={`${text} icon`}
-          />
-        </span>
-      </Link>
-
-      {text}
-    </div>
   );
 };
