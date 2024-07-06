14a4f81fbbb9516631e7aec09fa363d73965c8d1
Josue Zavala <josue.zavala.projects@gmail.com>
2024-07-05 20:30:37 -0600

    Finishing Sales mobile Activity, [WIP] week period

--- src/components/modules/activites/Mobile/ActivitiesTables/Sales/Table/index.tsx
+++ src/components/modules/activites/Mobile/ActivitiesTables/Sales/Table/index.tsx
@@ -1,72 +1,25 @@
-import { useEffect, useState } from "react";
-
-import dayjs from "dayjs";
-
 import { DataTable } from "@/components/DataTable";
-import { SalesModal } from "@/components/modules/activites/Desktop/sales/modals/deleteModal";
-import { useSalesTables } from "@/hooks/useSalesTables";
-import { TimeOption } from "@/types/activities";
+import { Sale } from "@/types/sales";
 
 import { SalesMobileColumns } from "./columns";
-import { useDates } from "@/hooks/useDates";
 
-type datesType = { startDate?: string; endDate?: string };
+type SalesActivityTableProps = {
+  sales: Sale[];
+  handleEditSale: (id: string) => Promise<void>;
+  handleShowModal: (id: string) => void;
+};
 
 export const SalesActivityTable = ({
-  timeOptionSelected,
-}: {
-  timeOptionSelected: TimeOption;
-}) => {
-  const [showModal, setShowModal] = useState<boolean>(false);
-  const {
-    isLoadingModal,
-    modalStep,
-    sales,
-    setModalStep,
-    setSaleId,
-    HandleEditSale,
-    HandleDeleteSale,
-    HandleGetSalesByDate,
-  } = useSalesTables();
-  const { GetCurrentDayDates, GetCurrentMonthDates, GetCurrentYearDates } =
-    useDates();
-
-  dayjs.locale("es");
-
-  const handleShowModal = (id: string) => {
-    setSaleId(id);
-    setShowModal(true);
-  };
-
-  const handleSetDates = () => {
-    if (timeOptionSelected === TimeOption.day) return GetCurrentDayDates();
-    if (timeOptionSelected === TimeOption.month) return GetCurrentMonthDates();
-    if (timeOptionSelected === TimeOption.year) return GetCurrentYearDates();
-
-    return { startDate: "", endDate: "" };
-  };
-
-  useEffect(() => {
-    const { startDate, endDate }: datesType = handleSetDates();
-    if (!startDate && !endDate) return;
-
-    HandleGetSalesByDate(startDate, endDate);
-  }, [timeOptionSelected]);
-
+  sales,
+  handleEditSale,
+  handleShowModal,
+}: SalesActivityTableProps) => {
   return (
     <>
-      <SalesModal
-        showModal={showModal}
-        setShowModal={setShowModal}
-        modalStep={modalStep}
-        deleteSale={HandleDeleteSale}
-        isLoading={isLoadingModal}
-        setModalStep={setModalStep}
-      />
       <DataTable
         columns={SalesMobileColumns({
           handleDeleteSale: handleShowModal,
-          handleEditSale: HandleEditSale,
+          handleEditSale: handleEditSale,
         })}
         data={sales}
       />
--- src/components/modules/activites/Mobile/ActivitiesTables/Sales/index.tsx
+++ src/components/modules/activites/Mobile/ActivitiesTables/Sales/index.tsx
@@ -1,20 +1,75 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { ContainerCard } from "@/components/ui/containerCard";
+import { useDates } from "@/hooks/useDates";
+import { useSalesTables } from "@/hooks/useSalesTables";
 import { LabelText, TimeOption, colorLabel } from "@/types/activities";
 
+import { SalesModal } from "../../../Desktop/sales/modals/deleteModal";
 import { SummaryLabel } from "../../components/SummaryLabel";
 import { TimeOptions } from "../../components/TimeOptions";
 import { SalesActivityTable } from "./Table";
 
+type datesType = { startDate?: string; endDate?: string };
+
 export const SalesActivity = () => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [totalSales, setTotalSales] = useState<number>(0);
   const [timeOptionSelected, setTimeOptionSelected] = useState<TimeOption>(
     TimeOption.day
   );
 
+  const {
+    isLoadingModal,
+    modalStep,
+    sales,
+    setModalStep,
+    setSaleId,
+    HandleEditSale,
+    HandleDeleteSale,
+    HandleGetSalesByDate,
+  } = useSalesTables();
+  const { GetCurrentDayDates, GetCurrentMonthDates, GetCurrentYearDates } =
+    useDates();
+
+  const handleShowModal = (id: string) => {
+    setSaleId(id);
+    setShowModal(true);
+  };
+
+  const handleSetDates = () => {
+    if (timeOptionSelected === TimeOption.day) return GetCurrentDayDates();
+    if (timeOptionSelected === TimeOption.month) return GetCurrentMonthDates();
+    if (timeOptionSelected === TimeOption.year) return GetCurrentYearDates();
+
+    return { startDate: "", endDate: "" };
+  };
+
+  const handleCalculateTotalSales = () =>
+    sales.length ? sales.map((sale) => sale.total).reduce((a, b) => a + b) : 0;
+
+  useEffect(() => {
+    setTotalSales(handleCalculateTotalSales());
+  }, [sales]);
+
+  useEffect(() => {
+    const { startDate, endDate }: datesType = handleSetDates();
+    if (!startDate && !endDate) return;
+
+    HandleGetSalesByDate(startDate, endDate);
+  }, [timeOptionSelected]);
+
   return (
     <ContainerCard>
+      <SalesModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        modalStep={modalStep}
+        deleteSale={HandleDeleteSale}
+        isLoading={isLoadingModal}
+        setModalStep={setModalStep}
+      />
       <div className="flex flex-col text-center text-2xl gap-6">
         <div className="text-main-blue font-bold">Ventas del día</div>
         <TimeOptions
@@ -24,10 +79,14 @@ export const SalesActivity = () => {
         <SummaryLabel
           text={`Ventas ${LabelText[timeOptionSelected]}`}
           color={colorLabel.green}
-          amount={800}
+          amount={totalSales}
         />
 
-        <SalesActivityTable timeOptionSelected={timeOptionSelected} />
+        <SalesActivityTable
+          sales={sales}
+          handleEditSale={HandleEditSale}
+          handleShowModal={handleShowModal}
+        />
       </div>
     </ContainerCard>
   );
