5611482ea61a55b52e97ed345cddeeb129bb5bcd
Josue Zavala <josue.zavala.projects@gmail.com>
2024-07-05 19:20:54 -0600

    Creating useSalesTable hook

--- src/components/modules/activites/Desktop/sales/index.tsx
+++ src/components/modules/activites/Desktop/sales/index.tsx
@@ -1,76 +1,42 @@
 "use client";
 import { useEffect, useState } from "react";
 
-import { setCookie } from "cookies-next";
 import dayjs from "dayjs";
-import { DocumentData } from "firebase/firestore";
-import { useRouter } from "next/navigation";
 
-import { data as SalesData } from "@/constants/activities/sales";
-import { useSales } from "@/hooks/useSales";
-import { Sale, SalesModalStep } from "@/types/sales";
+import { useSalesTables } from "@/hooks/useSalesTables";
 
 import { DataTable } from "../../../../DataTable";
-import useActivitiesDateState from "../states/activities-dates-state";
 import { SalesModal } from "./modals/deleteModal";
 import { SalesColumns } from "./table/columns";
 
 require("dayjs/locale/es");
 
 export const SalesTable = () => {
-  const [sales, setSales] = useState<Sale[]>(SalesData);
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [isLoadingModal, setIsLoadingModal] = useState<boolean>(false);
-  const [saleId, setSaleId] = useState<string>("");
-  const [modalStep, setModalStep] = useState<SalesModalStep>(
-    SalesModalStep.delete
-  );
+  const {
+    isLoadingModal,
+    modalStep,
+    startDate,
+    endDate,
+    sales,
+    setModalStep,
+    setSaleId,
+    HandleEditSale,
+    HandleDeleteSale,
+    HandleGetSalesByDate,
+  } = useSalesTables();
 
-  const { DeleteSale, GetSalesByDate, GetSaleByID } = useSales();
-
-  const { startDate, endDate } = useActivitiesDateState();
-
-  const router = useRouter();
   dayjs.locale("es");
-
-  const handleGetSalesByDate = async (startDate: string, endDate: string) => {
-    const result = await GetSalesByDate(startDate, endDate);
-
-    setSales(result);
-  };
-
-  const handleEditSale = async (id: string) => {
-    const saleReponse: DocumentData | undefined = await GetSaleByID(id);
-    const { products } = saleReponse || {};
-
-    setCookie("products", JSON.stringify(products));
-    setCookie("saleID", id);
-    router.push("/salesPoint");
-  };
 
   const handleShowModal = (id: string) => {
     setSaleId(id);
     setShowModal(true);
   };
 
-  const handleDeleteSale = async () => {
-    setIsLoadingModal(true);
-    try {
-      const response = await DeleteSale(saleId);
-      setModalStep(SalesModalStep.deleteConfirm);
-
-      handleGetSalesByDate(startDate, endDate);
-    } catch {
-      throw new Error("Something went wrong");
-    } finally {
-      setIsLoadingModal(false);
-    }
-  };
-
   useEffect(() => {
     if (!startDate && !endDate) return;
 
-    handleGetSalesByDate(startDate, endDate);
+    HandleGetSalesByDate(startDate, endDate);
   }, [startDate, endDate]);
 
   return (
@@ -79,14 +45,14 @@ export const SalesTable = () => {
         showModal={showModal}
         setShowModal={setShowModal}
         modalStep={modalStep}
-        deleteSale={handleDeleteSale}
+        deleteSale={HandleDeleteSale}
         isLoading={isLoadingModal}
         setModalStep={setModalStep}
       />
       <DataTable
         columns={SalesColumns({
           handleDeleteSale: handleShowModal,
-          handleEditSale,
+          handleEditSale: HandleEditSale,
         })}
         data={sales}
       />
