63b1c0ded7d11b74f34fd8b6185f55bf0242c049
Josue Zavala <josue.zavala.projects@gmail.com>
2024-07-09 00:35:52 -0600

    Correcting Product styling

--- src/components/modules/product/components/ProductDetails/ProductImage.tsx
+++ src/components/modules/product/components/ProductDetails/ProductImage.tsx
@@ -10,8 +10,8 @@ type ProductImageProps = {
 
 export const ProductImage = ({ handleShowDeleteModal }: ProductImageProps) => {
   return (
-    <section className="flex flex-col gap-16 items-end w-1/2 pr-8">
-      <section className="grid justify-items-center items-center w-1/2">
+    <section className="flex flex-col gap-16 sm:items-end sm:w-1/2 sm:pr-8">
+      <section className="grid justify-items-center items-center sm:w-1/2">
         <span className="grid justify-items-center items-center p-4 border-dashed border-2 border-secondary-gray">
           <span className="w-[150px] h-[150px]"></span>
           {/* TODO: Add Imagen */}
@@ -23,13 +23,13 @@ export const ProductImage = ({ handleShowDeleteModal }: ProductImageProps) => {
           /> */}
         </span>
       </section>
-      <section className="flex flex-col gap-4 w-1/2">
+      <section className="flex flex-col gap-4 sm:w-1/2">
         <InformationCard title="Existencias Iniciales" data="30" />
         <InformationCard title="Inventario Restante" data="34" />
         <InformationCard title="En Camino" data="15" />
         <InformationCard title="Precio de Venta" data="12" />
       </section>
-      <section className="flex flex-col items-center gap-4 w-1/2">
+      <section className="flex flex-col items-center gap-4 sm:w-1/2">
         <span
           onClick={() => handleShowDeleteModal()}
           className="flex content-center items-center gap-2 text-main-red cursor-pointer"
