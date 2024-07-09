f02b3fa0499130a008a95db9ffce0289eec51351
Josue Zavala <josue.zavala.projects@gmail.com>
2024-07-09 00:53:13 -0600

    Adding onClick to CartResume

--- src/components/modules/salesPoint/Mobile/order/components/cartResume/index.tsx
+++ src/components/modules/salesPoint/Mobile/order/components/cartResume/index.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
 
 import useSalesPointState from "@/components/modules/salesPoint/states/sales-point-state";
+import { TAB_KEYS } from "@/constants/salesPoint";
 import { formatCurrency } from "@/utils/common";
 
 import { CartIcon } from "../../../../../../../../public/assets";
 
 export const CartResume = () => {
-  const { products, total } = useSalesPointState();
+  const { products, total, setTabName } = useSalesPointState();
   return (
     <>
       {Boolean(products.length) && (
-        <div className="flex flex-col sm:flex-row justify-between w-full bg-gradient-to-r from-main-blue to-light-blue text-white py-4 px-8 rounded-2xl">
+        <div
+          className="flex flex-col sm:flex-row justify-between w-full bg-gradient-to-r from-main-blue to-light-blue text-white py-4 px-8 rounded-2xl cursor-pointer"
+          onClick={() => setTabName(TAB_KEYS.TOTAL)}
+        >
           <div className="flex justify-center gap-4">
             <span className="flex justify-center">
               <Image src={CartIcon} alt="icon" />
