78d9f3bb89099cdb2a27f6822d5c27190b182abc
Josue Zavala <josue.zavala.projects@gmail.com>
2024-08-21 16:15:28 -0600

    Correcting isPermiumUser function

--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { getCookie } from "cookies-next";
+import { getCookie, setCookie } from "cookies-next";
 
 import { useRouter } from "next/navigation";
 
@@ -8,13 +8,14 @@ import SideNavbar from "@/components/SideNavLegacy/SideNavbar";
 import AuthContext from "@/context/AuthContext";
 import { useSubscription } from "@/hooks/useSubscription";
 import { initFirebase } from "@/services/firebase";
-import { getCancelPeriodEnd } from "@/services/stripePayments";
+import { getCancelPeriodEnd, getCheckoutUrl } from "@/services/stripePayments";
 
 export const LogguedPortal = ({ children }: { children: React.ReactNode }) => {
   const authCtx = useContext(AuthContext);
   const [isValid, setIsvalid] = useState(false);
   const { IsValidSubscription } = useSubscription();
   const router = useRouter();
+
   const app = initFirebase();
 
   const getCookiesLogin = () => {
@@ -23,22 +24,39 @@ export const LogguedPortal = ({ children }: { children: React.ReactNode }) => {
       localId: getCookie("token"),
       expirationTime: getCookie("expirationTime"),
       isPremium: getCookie("isPremium"),
+      sentToStripe: getCookie("sentToStripe"),
     };
   };
 
+  const upgradeToPremium = async () => {
+    const checkoutUrl = await getCheckoutUrl(app);
+    /* authCtx.logout(); */
+    router.push(checkoutUrl);
+    console.log("Upgrade to Premium");
+  };
+
   const isPremiumUser = async () => {
-    const { email } = getCookiesLogin();
+    const { email, sentToStripe } = getCookiesLogin();
+
+    if (sentToStripe) {
+      authCtx.logout();
+      router.refresh();
+      return;
+    }
 
     try {
       if (email) {
         const response = await getCancelPeriodEnd(app);
 
         if (!IsValidSubscription(response)) {
+          // authCtx.logout();
+          setCookie("sentToStripe", true);
+          upgradeToPremium();
           setIsvalid(false);
-          authCtx.logout();
           router.refresh();
           return;
         }
+        setCookie("sentToStripe", false);
         setIsvalid(true);
       }
     } catch {
